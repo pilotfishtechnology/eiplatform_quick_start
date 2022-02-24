2c97de22384672b0e67bced9d600ca747fc9345e
Danie <ddekock@pilotfishtechnology.com>
2022-02-24 12:28:55 +0200

    Updated example files

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
-docker run -it -d --rm --name eiplatform -v $(pwd)/logs:/opt/pilotfish/logs -v $(pwd)/pflicense.key:/opt/pilotfish/license/pflicense.key -v $(pwd)/eip-root:/opt/pilotfish/eip-root -p 8080:8080 pilotfishtechnology/eiplatform
+#!/bin/bash
+
+docker run -it -d --rm --name eiplatform \
+  -v $(pwd)/logs:/opt/pilotfish/logs \
+  -v $(pwd)/pflicense.key:/opt/pilotfish/license/pflicense.key \
+  -v $(pwd)/eip-root:/opt/pilotfish/eip-root \
+  -p 8080:8080 \
+  pilotfishtechnology/eiplatform
---
+++ upgrade.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sh stop.sh
+docker pull pilotfishtechnology/eiplatform
+sh start.sh
